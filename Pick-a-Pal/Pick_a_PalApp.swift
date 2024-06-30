af3ca29d1f63f6814394fb11ec8d92d690a416a9
Cindy7878 <cindy000708@gmail.com>
2024-06-30 13:49:44 +0900

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by SOOKYUNG CHO on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
